b5f028b84c78edc1f7e04cc14a1426134c783f73
Ben Bajek <ben.bajek@i2n.com>
2020-04-16 14:59:37 +0100

    Added inspector.yaml

--- python/cfn-app-svc-manager.py
+++ python/cfn-app-svc-manager.py
@@ -6,7 +6,7 @@ import json
 import subprocess
 
 class cfnAppServiceManager:
-    def __init__(self, **kwargs)
+    def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
     def buildPropertiesDict(self):
